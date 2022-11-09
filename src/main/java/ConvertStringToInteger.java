6d13ea1008523b6e8df3573dd099d0856a894878
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-11-09 19:42:25 +0330

    update

--- src/main/java/ConvertStringToInteger.java
+++ src/main/java/ConvertStringToInteger.java
@@ -1,12 +1,11 @@
 public class ConvertStringToInteger {
-    public static int convertStringToInteger(String value){
+    public static int convertStringToInteger(String value) {
         int number;
-        if(value.matches("-?[1-9]\\d*|0")){
-            number=Integer.valueOf(value);
-        }
-        else
+        if (value.matches("-?[1-9]\\d*|0")) {
+            number = Integer.valueOf(value);
+        } else
             throw new NumberFormatException("please just enter digit");
-        if(number>(-32767)&&number<32767)
+        if (number > (-32767) && number < 32767)
             return number;
         else
             throw new IllegalArgumentException("number nut in range");
--- src/test/java/ConvertStringToIntegerTest.java
+++ src/test/java/ConvertStringToIntegerTest.java
@@ -5,24 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConvertStringToIntegerTest {
     @Test
     void convertStringToIntegerTest() {
-        String value="1234";
-        int number=ConvertStringToInteger.convertStringToInteger(value);
-        assertEquals(1234,number);
+        String value = "0";
+        int number = ConvertStringToInteger.convertStringToInteger(value);
+        assertEquals(0, number);
     }
+
     @Test
-    void inputTest(){
-        try{
-        int number=ConvertStringToInteger.convertStringToInteger("12sd");}
-        catch (NumberFormatException e){
-            assertEquals("please just enter digit",e.getMessage());
+    void inputTest() {
+        try {
+            int number = ConvertStringToInteger.convertStringToInteger("12sd");
+        } catch (NumberFormatException e) {
+            assertEquals("please just enter digit", e.getMessage());
         }
     }
+
     @Test
-    void inputBound(){
-        try{
-            int number=ConvertStringToInteger.convertStringToInteger("327862");}
-        catch (IllegalArgumentException e){
-            assertEquals("number nut in range",e.getMessage());
+    void inputBound() {
+        try {
+            int number = ConvertStringToInteger.convertStringToInteger("327862");
+        } catch (IllegalArgumentException e) {
+            assertEquals("number nut in range", e.getMessage());
         }
     }
 }
